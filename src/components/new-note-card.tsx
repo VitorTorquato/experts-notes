d78af0e459641e736ca21b0c0667f02f00341d26
Vitor <torquatovitor68@gmail.com>
2024-02-08 13:41:36 +0100

    building

---
+++ src/components/new-note-card.tsx
@@ -0,0 +1,10 @@
+export function NewNoteCard() {
+
+return (
+    <div className='rounded-md bg-slate-700 p-5 space-y-3'>
+    <span className='text-sm font-medium text-slate-300'>Adicionar nota</span>
+    <p className='text-sm leading-6 text-slate-200'>Grave uma nota em audio que será convertido para texto automaticamente </p>
+</div>
+
+)
+};
